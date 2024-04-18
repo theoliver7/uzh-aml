7392400f64787bad96a9df2aa58adda8bf9991fb
Marco Heiniger <marcoh228@gmail.com>
2024-04-18 18:16:18 +0200

    update SPK and modifiyNodeInfScore

---
+++ models/hgp_sl_py311_modifyNodeInfScore/README.md
@@ -0,0 +1,5 @@
+# Euclidean distance
+gave 73.1 % accuracy on test set
+
+# Manhattan distance
+74.78% accuracy
--- models/sp_kernel_baseline/shortest_path_kernel.py
+++ models/sp_kernel_baseline/shortest_path_kernel.py
@@ -1,165 +1,36 @@
-import numpy as np
+from grakel.utils import graph_from_networkx
 from sklearn.svm import SVC
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from gklearn.kernels import spkernel 
 from sklearn.metrics import accuracy_score
+from grakel.kernels import ShortestPath
+from grakel import GraphKernel, datasets
 import pickle
-import multiprocessing
-import networkx as nx
-def load():
-    with open("graphs.pickle", "rb") as f:
-        graphs = pickle.load(f)
-    with open("labels.pickle", "rb") as f:
-        labels = pickle.load(f)
-    return graphs, labels
+#DD_data = datasets.fetch_dataset("DD", verbose=False)
+#DD_d = DD_data.data
+#DD_t = DD_data.target
 
-
-
-class GK_SP:
-    """
-    Shorthest path graph kernel.
-    """
-    def compare(self, g_1, g_2, verbose=False):
-        """Compute the kernel value (similarity) between two graphs.
-
-        Parameters
-        ----------
-        g1 : networkx.Graph
-            First graph.
-        g2 : networkx.Graph
-            Second graph.
-
-        Returns
-        -------
-        k : The similarity value between g1 and g2.
-        """
-        # Diagonal superior matrix of the floyd warshall shortest
-        # paths:
-        fwm1 = np.array(nx.floyd_warshall_numpy(g_1))
-        fwm1 = np.where(fwm1 == np.inf, 0, fwm1)
-        fwm1 = np.where(fwm1 == np.nan, 0, fwm1)
-        fwm1 = np.triu(fwm1, k=1)
-        bc1 = np.bincount(fwm1.reshape(-1).astype(int))
-
-        fwm2 = np.array(nx.floyd_warshall_numpy(g_2))
-        fwm2 = np.where(fwm2 == np.inf, 0, fwm2)
-        fwm2 = np.where(fwm2 == np.nan, 0, fwm2)
-        fwm2 = np.triu(fwm2, k=1)
-        bc2 = np.bincount(fwm2.reshape(-1).astype(int))
-
-        # Copy into arrays with the same length the non-zero shortests
-        # paths:
-        v1 = np.zeros(max(len(bc1), len(bc2)) - 1)
-        v1[range(0, len(bc1)-1)] = bc1[1:]
-
-        v2 = np.zeros(max(len(bc1), len(bc2)) - 1)
-        v2[range(0, len(bc2)-1)] = bc2[1:]
-
-        return np.sum(v1 * v2)
-
-    def compare_normalized(self, g_1, g_2, verbose=False):
-        """Compute the normalized kernel value between two graphs.
-
-        A normalized version of the kernel is given by the equation:
-        k_norm(g1, g2) = k(g1, g2) / sqrt(k(g1,g1) * k(g2,g2))
-
-        Parameters
-        ----------
-        g1 : networkx.Graph
-            First graph.
-        g2 : networkx.Graph
-            Second graph.
-
-        Returns
-        -------
-        k : The similarity value between g1 and g2.
-
-        """
-        return self.compare(g_1, g_2) / (np.sqrt(self.compare(g_1, g_1) *
-                                                 self.compare(g_2, g_2)))
-
-    def compare_list2(self, graph_list,graph_list2, verbose=False):
-        """Compute the all-pairs kernel values for a list of graphs.
-
-        This function can be used to directly compute the kernel
-        matrix for a list of graphs. The direct computation of the
-        kernel matrix is faster than the computation of all individual
-        pairwise kernel values.
-
-        Parameters
-        ----------
-        graph_list: list
-            A list of graphs (list of networkx graphs)
-
-        Return
-        ------
-        K: numpy.array, shape = (len(graph_list), len(graph_list))
-        The similarity matrix of all graphs in graph_list.
-
-        """
-        n = len(graph_list)
-        m = len(graph_list2)
-        k = np.zeros((n, m))
-        for i in range(n):
-            for j in range(i, m):
-                k[i, j] = self.compare(graph_list[i], graph_list2[j])
-                k[j, i] = k[i, j]
-
-        k_norm = np.zeros(k.shape)
-        for i in range(k.shape[0]):
-            for j in range(k.shape[1]):
-                k_norm[i, j] = k[i, j] / np.sqrt(k[i, i] * k[j, j])
-
-        return k_norm
-  
-
-    def compare_list(self, graph_list1, graph_list2, verbose=False):
-        """
-        angepasste funktion
-        """
-        n = len(graph_list1)
-        m = len(graph_list2)
-        k = np.zeros((n, m))
-
-        for i in range(n):
-            for j in range(m):
-                k[i, j] = self.compare(graph_list1[i], graph_list2[j])
-
-        k_norm = np.zeros(k.shape)
-        for i in range(min(n, m)):
-            for j in range(min(n, m)):
-                k_norm[i, j] = k[i, j] / np.sqrt(k[i, i] * k[j, j])
-
-        return k_norm
-
-
-if __name__ == '__main__':
-    graphs,labels= load()
-    graphs =list(graphs.values())
-    graphs = graphs[680:700]
-    labels = labels[680:700]
-    
-    # Train-test split
-    X_train, X_test, y_train, y_test = train_test_split(graphs, labels, test_size=0.2, random_state=42)
-    
-    # Compute shortest path kernels using graphkit-learn
-    shortest_path = GK_SP()
-
-    shortest_path_kernel_train, _ , _ = spkernel(X_train,n_jobs=1) # funktioniert nicht mit spkernel
-    shortest_path_kernel_test,_ , _ = spkernel(X_test, X_train,n_jobs=1)
-    # shortest_path_kernel_train = shortest_path.compare_list(X_train,X_train)
-    # shortest_path_kernel_test = shortest_path.compare_list(X_test,X_train)
-    #
-    print("created kernel")
-
-    # Create and train an SVM classifier
-    clf = SVC(kernel='precomputed')  # Use precomputed kernel
-    clf.fit(shortest_path_kernel_train, y_train)
-    print("finished training")
+with open(r"models\sp_kernel_baseline\graphs.pickle", "rb") as f:
+    graphs = pickle.load(f)
+with open(r"models\sp_kernel_baseline\labels.pickle", "rb") as f:
+    labels = pickle.load(f)
    
-    y_pred = clf.predict(shortest_path_kernel_test)
-    print("finished predicting")
-   
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Accuracy:", accuracy)
+graphs_g = list(graph_from_networkx(list(graphs.values())))
+
+# Split data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(graphs_g, labels, test_size=0.2, random_state=42)
+
+# Define kernel
+kernel = ShortestPath(normalize=True,with_labels=False)
+
+# Compute kernel matrices
+K_train = kernel.fit_transform(X_train)
+K_test = kernel.transform(X_test)
+
+# Train SVM with the computed kernel
+svm = SVC(kernel='precomputed')
+svm.fit(K_train, y_train)
+
+# Predictions
+y_pred = svm.predict(K_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy:.4f}")
